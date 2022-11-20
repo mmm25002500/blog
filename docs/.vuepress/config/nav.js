359f4ca1af1af7a8402862e832f73d64eba39f43
mmm25002500 <marryhan25648@gmail.com>
2022-11-20 20:57:53 +0800

    Upload Files

--- docs/.vuepress/config/nav.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  {text: '首页', link: '/'},
-  {text: '分类', link: '/categories/'},
-  {text: '标签', link: '/tags/'},
-  {text: '关于', link: '/about/'},
-  {text: '友链', link: '/friends/'},
-  {text: '归档', link: '/archives/'}
-]
